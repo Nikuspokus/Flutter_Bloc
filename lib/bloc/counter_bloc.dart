1c8cf559f94110c8bc7300b28b9472593880b8aa
Nicolas Marquillier <nicolas.marquillier@jpvelowks014.elosi.lan>
2020-01-28 11:55:05 +0100

    Stream les entrees

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import 'bloc.dart';
 
 class CounterBloc extends Bloc {
 
-  @override
-  dispose() {
-    // TODO: implement dispose
-    return null;
+  int counter = 0;
+
+  //StreamController
+  final _streamController = StreamController<int>();
+
+  //Entrées
+  Sink<int> get sink => _streamController.sink;
+
+  //Sorties
+
+  //Logique de travail
+  incrementCounter(){
+    counter += 1;
+    sink.add(counter);
   }
+
+  //Fermeture du bloc
+  @override
+  dispose() => _streamController.close();
+
 }
